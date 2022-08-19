8632fa64b24f22743e89444fd6e165802ce06bbf
unknown <bhatkrati@gmail.com>
2022-08-19 16:37:25 +0530

    Sample Commit

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "BornElectricVision",
+    shares: 97,
+  },
+  {
+    name: "Avengers",
+    shares: 60.3,
+  },
+  {
+    name: "HappyCoding",
+    shares: 75.5,
+  },
+  {
+    name: "First MV",
+    shares: 63,
+  },
+  {
+    name: "Elon Musk",
+    shares: 78.9,
+  },
+  {
+    name: "Bunny",
+    shares: 100,
+  },
+];
